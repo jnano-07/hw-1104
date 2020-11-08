150a2a58ff7004d42b24e1c096ed1fa6a7d3df7a
jnano-07 <71751213+jnano-07@users.noreply.github.com>
2020-11-08 21:45:54 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,8 @@ basic.forever(function () {
     for (let x = 0; x <= 4; x++) {
         for (let y = 0; y <= x; y++) {
             led.plot(4 - x, y)
+        }
+        for (let y = 0; y <= x - 1; y++) {
             led.plot(4 - y, x)
         }
         basic.pause(200)
